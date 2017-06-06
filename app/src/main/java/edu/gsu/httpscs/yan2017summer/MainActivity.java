e488ebc0d888e8dc36e29fdfe038246512bf5755
Tong Tian <ttian4@student.gsu.edu>
2017-06-06 15:06:37 -0400

    Initial commit

---
+++ app/src/main/java/edu/gsu/httpscs/yan2017summer/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.gsu.httpscs.yan2017summer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
